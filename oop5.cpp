2adf7df81f5ec8b21479bd9736b7f17d9dfc0cd5
azamatshsh123@gmail.com <azamatshsh123@gmail.com>
2023-03-28 13:54:18 +0300

    part 1

--- oop5.cpp
+++ oop5.cpp
@@ -1,32 +1,43 @@
 ﻿#include<iostream>
 class Prototype {//родительский класс
 public:
-	Prototype();//конструктор
-	virtual ~Prototype();//виртуальный деструктор
+	/*Prototype();*///конструктор
+	/*virtual ~Prototype();виртуальный деструктор*/
 	void doSomething1() {//невиртуальный метод 1
 		std::cout << "Prototype::doSomething1" << std::endl;
 		doSomething3();
 	}
 	void doSomething2() {//невиртуальный метод 2
-		std::cout << "Prototype::doSomething2" << std::endl;
+		std::cout << "Prototype::doSomething1" << std::endl;
 		doSomething4();
 	}
-	void doSomething3() {//виртуальный метод 3
-		std::cout << "Prototype::doSomething3" << std::endl;
+	void doSomething3() {//невиртуальный метод 3
+		std::cout << "Prototype::doSomething2 non virtual" << std::endl;
 	}
 	virtual void doSomething4() {//виртуальный метод 4
-		std::cout << "Prototype::doSomething4" << std::endl;
+		std::cout << "Prototype::doSomething2 virtual" << std::endl;
 	}
-	virtual Prototype* clone() = 0;//метод создания клонов согласно паттерну Prototype
 	virtual std::string classname() {//метод возвращения имени класс объекта
 		return "Prototype";
 	}
-	virtual bool IsA(std::string classname) {//метод для предварительной проверки при работе  с небезопасным приведением типов
-
+	/*virtual bool IsA(std::string classname);*///метод для предварительной проверки при работе  с небезопасным приведением типов
+};
+class Daughter  : public Prototype {//Дочерний класс от класса-прототипа
+public:
+	
+	void doSomething3() {//невиртуальный метод 3
+		std::cout << "Daughter::doSomething2 non virtual" << std::endl;
+	}
+	void doSomething4() override{//виртуальный метод 4
+		std::cout << "Daughter::doSomething2 virtual" << std::endl;
 	}
 };
-class Daughter  : public Prototype{//Дочерний класс от класса-прототипа
-public:
-	Daughter() : Prototype() {};
-
-};
+int main() {
+	Daughter testDaughter;
+	std::cout << "Result of Daughter::doSomething1() and non virtual method2: " << std::endl;
+	testDaughter.doSomething1();
+	std::cout << std::endl;
+	std::cout << "Result of Daughter::doSomething1() and virtual method2: " << std::endl;
+	testDaughter.doSomething2();
+	return 0;
+}

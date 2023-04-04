1807def7e0510a94b6891e5aead4ff201a23e74f
azamatshsh123@gmail.com <azamatshsh123@gmail.com>
2023-04-04 16:36:28 +0300

    finalvers

--- oop5.cpp
+++ oop5.cpp
@@ -2,16 +2,16 @@
 class Base {//родительский класс
 public:
 	Base() {
-
+		std::cout << "Base()" << std::endl;
 	}
 	Base(Base* obj) {
-
+		std::cout << "Base(Base* obj)" << std::endl;
 	}
 	Base(Base& obj) {
-
+		std::cout << "Base(Base& obj)" << std::endl;
 	}
-	~Base() {
-
+	virtual ~Base() {
+		std::cout << "~Base()" << std::endl;
 	}
 	void doSomething1() {//невиртуальный метод 1
 		std::cout << "Base::doSomething1" << std::endl;
@@ -30,21 +30,23 @@ public:
 	virtual std::string classname() {//метод возвращения имени класс объекта
 		return "Base";
 	}
-	/*virtual bool IsA(std::string classname);*///метод для предварительной проверки при работе  с небезопасным приведением типов
+	virtual bool IsA(std::string classname) {//метод для предварительной проверки при приведении типа
+		return classname == "Base";
+	}
 };
 class Desk  : public Base {//Дочерний класс
 public:
 	Desk(){
-
+		std::cout << "Desk()" << std::endl;
 	}
 	Desk(Desk* obj){
-
+		std::cout << "Desk(Desk* obj)" << std::endl;
 	}
 	Desk(Desk& obj){
-
+		std::cout << "Desk(Desk& obj)" << std::endl;
 	}
-	~Desk() {
-
+	virtual ~Desk() {
+		std::cout << "~Desk()" << std::endl;
 	}
 	void doSomething3() {//невиртуальный метод 3
 		std::cout << "Desk::doSomething2 non virtual" << std::endl;
@@ -55,13 +57,124 @@ public:
 	std::string classname() override {
 		return "Desk";
 	}
+	bool IsA(std::string classname) override {
+		return classname == "Desk";
+	}
 };
+void func() {
+	std::cout << "func()" << std::endl;
+}
+void func(Base* obj) {
+	std::cout << "func(Base* obj)" << std::endl;
+	(static_cast<Desk*>(obj))->doSomething3();
+}
+void func(Base& obj) {
+	std::cout << "func(Base& obj)" << std::endl;
+	(static_cast<Desk*>(&obj))->doSomething3();
+}
+Base func1() {
+	Base local;
+	return local;
+}
+Base* func2() {
+	Base local;
+	return &local;
+}
+Base& func3() {
+	Base local;
+	return local;
+}
+Base func4() {
+	Base* local = new Base();
+	return *local;
+}
+Base* func5() {
+	Base* local = new Base();
+	return local;
+}
+Base& func6() {
+	Base* local = new Base();
+	return *local;
+}
 int main() {
-	Desk testDaughter;
-	std::cout << "Result of Desk::doSomething1() and non virtual method2: " << std::endl;
-	testDaughter.doSomething1();
-	std::cout << std::endl;
-	std::cout << "Result of Desk::doSomething1() and virtual method2: " << std::endl;
-	testDaughter.doSomething2();
+	std::cout << "1)\n{\n";//жизненный цикл unique_ptr
+	std::cout << "std::unique_ptr<Base> basa(new Base())" << std::endl;
+	{
+		std::unique_ptr <Base> basa(new Base());
+	}
+	std::cout << "}\n";
+	std::cout << "2)\n{\n";//жизненный цикл shared_ptr
+	std::cout << "std::shared_ptr<Base> basa(new Base())" << std::endl;
+	{
+		std::shared_ptr <Base> basa(new Base());
+		{
+			std::shared_ptr<Base> basa1=basa;
+			std::cout << basa << std::endl << basa1<<std::endl;
+		}
+	}
+	std::cout << "}\n";
+	std::cout << "3)\n{\n";//небезопасное приведение типов
+	{
+		std::cout << "(static_cast<Desk*>(stcast))->doSomething3()" << std::endl;
+		Base* stcast = new Desk();
+		if (stcast->IsA("Desk")) {
+			(static_cast<Desk*>(stcast))->doSomething3();
+		}
+		delete stcast;
+	}
+	std::cout << "}\n";
+	std::cout << "4)\n{\n";//безопасное приведение типов
+	{
+		std::cout << "(dynamic_cast<Desk*>(dcast))->doSomething3()" << std::endl;
+		Base* dcast = new Desk();
+		Desk* c = dynamic_cast<Desk*>(dcast);
+		if (c != nullptr) {
+			c->doSomething3();
+		}
+		delete dcast;
+	}
+	std::cout << "}\n";
+	std::cout << "5)\n{\n";//вызов виртуальных и невиртуальных методов в дочернем объекте
+	{
+		Desk testDaughter;
+		std::cout << "Result of Desk::doSomething1() and non virtual method2: " << std::endl;
+		testDaughter.doSomething1();
+		std::cout << "Result of Desk::doSomething1() and virtual method2: " << std::endl;
+		testDaughter.doSomething2();
+	}
+	std::cout << "}\n";
+	std::cout << "6)\n{\n";//передача объектов в качестве параметров
+	{
+		std::unique_ptr <Base> test(new Desk());
+		func(*test);
+		func(test.get());
+	}
+	std::cout << "}\n";
+	std::cout << "7)\n{\n";//возврат созданных в функциях объектов
+	{
+		std::cout << "func1():\n";
+		Base b1;
+		b1 = func1();
+		
+		std::cout << "func2():\n";
+		Base *b2;
+		b2 = func2();
+		
+		std::cout << "func3():\n";
+		Base& b3=func3();
+		
+		std::cout << "func4():\n";
+		Base b4;
+		b4 = func4();
+		
+		std::cout << "func5():\n";
+		Base *b5;
+		b5 = func5();
+		
+		std::cout << "func6():\n";
+		Base& b6=func6();
+		
+	}
+	std::cout << "}\n";
 	return 0;
 }
